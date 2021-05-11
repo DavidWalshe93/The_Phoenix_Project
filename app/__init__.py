8f99058b261a7c88ffff2b71bcf3f1c64c48db04
DavidWalshe93 <david.walshe93@gmail.com>
2021-05-11 20:15:48 +0100

    Added a flask application factory function for creating multi configuration applications.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-
 """
 Author:     David Walshe
 Date:       10 May 2021
@@ -6,4 +5,39 @@ Date:       10 May 2021
 
 import logging
 
+from flask import Flask
+from flask.app import AppContext
+from flask_sqlalchemy import SQLAlchemy
+
+from configurations.env_setup import get_config
+
 logger = logging.getLogger(__name__)
+
+# Construct Flask extensions, initialise in factory function.
+db = SQLAlchemy()
+
+
+def create_app(config_name: str = "dev") -> Flask:
+    """
+    Factory function to create an application instance.
+
+    Allows various configurations to be injected to easily enable dev/test/prod instance creation.
+
+    :param config_name: The name of the configuration to use. Default: "dev".
+    :return: An application instance with the desired environment configuration settings.
+    """
+    app = Flask(__name__)
+
+    # Get environment configuration.
+    config = get_config(config_name)
+
+    # Inject configuration into application instance.
+    app.config.from_object(config)
+
+    # Initialise application configuration settings if required.
+    config.init_app(app)
+
+    # Initialise Database.
+    db.init_app(app)
+
+    return app
