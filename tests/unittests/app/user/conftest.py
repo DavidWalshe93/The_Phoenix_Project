a312ce19035b98a1d11738b0900d30b50a0b7a04
DavidWalshe93 <david.walshe93@gmail.com>
2021-05-12 22:55:49 +0100

    Created client object factory, added parameterised testing for listing users.

--- tests/unittests/app/user/conftest.py
+++ tests/unittests/app/user/conftest.py
@@ -21,13 +21,15 @@ def users() -> callable:
     :return: A callable for creating a list of user objects.
     """
 
-    def factory(datetime_as_string: bool = False, include_password: bool = False) -> List[Dict[str, Union[str, datetime]]]:
+    def factory(size: int = 3, datetime_as_string: bool = False, include_password: bool = False) -> List[Dict[str, Union[str, datetime]]]:
         """
+        :param size: The number of User dictionary instances to return.
         :param datetime_as_string: Convert datatime to string representation.
         :param include_password: Include user passwords in returned dictionary objects.
         :return: A list of user dictionaries.
         """
-        names = ["John Smith", "Mary Murphy", "Mike O'Shea"]
+        # Return only the number of instances requested by the user.
+        names = [name for idx, name in enumerate(["John Smith", "Mary Murphy", "Mike O'Shea"]) if idx < size]
 
         # Generate 'last_login" times.
         epoch = 1620766488  # Tue, 11 May 2021 21:54:48 GMT
@@ -38,8 +40,10 @@ def users() -> callable:
             date_format = "%a, %d %b %Y %H:%M:%S GMT"  # Tue, 11 May 2021 21:54:48 GMT
             times = [f"{time.strftime(date_format)}" for time in times]
 
+        # Create users
         users = [dict(name=user, email=f"{user}@example.com", last_login=time) for user, time in zip(names, times)]
 
+        # Enrich dictionary with user password information if required.
         if include_password:
             users = [dict(**user, password="".join(reversed(user["name"]))) for user in users]
 
@@ -57,12 +61,16 @@ def setup_db(users) -> callable:
     :return: A factory function to create a database.
     """
 
-    def _setup_db():
-        """Creates and populates a in-memory database for testing."""
+    def _setup_db(size: int = 3):
+        """
+        Creates and populates a in-memory database for testing.
+
+        :param size: The number of users to add to the database.
+        """
         # Creates tables in Database.
         db.create_all()
         # Create a list of User Models.
-        user_models = [User(**user) for user in users(include_password=True)]
+        user_models = [User(**user) for user in users(size=size, include_password=True)]
         # Add the User Models to the Database
         [db.session.add(user) for user in user_models]
         # Commit the changes.
@@ -72,17 +80,27 @@ def setup_db(users) -> callable:
 
 
 @pytest.fixture
-def client(setup_db: callable) -> Client:
+def client_factory(setup_db: callable) -> callable:
     """
     Supplies a flask client object to test the REST endpoints of the application.
 
     :param setup_db: Function to setup testing database.
-    :return: A Flask Client object.
+    :return: A factory function for creating a Flask Client object.
     """
-    app = create_app("test")
 
-    with app.test_client() as client:
-        with app.app_context():
-            setup_db()
+    def factory(size: int = 3) -> Client:
+        """
+        Creates and initialises a client object.
 
-        yield client
+        :param size: The number of instance to add to the database for testing.
+        :return: A Flask Client object.
+        """
+        app = create_app("test")
+
+        with app.test_client() as client:
+            with app.app_context():
+                setup_db(size=size)
+
+            return client
+
+    return factory
--- tests/unittests/app/user/test_endpoints.py
+++ tests/unittests/app/user/test_endpoints.py
@@ -9,17 +9,19 @@ import json
 from flask import Response
 
 
-def test_get_users(client, users):
+@pytest.mark.parametrize("size", [1, 2, 3])
+def test_get_users(size, client_factory, users):
     """
-    Validated a list of users is returned on a GET to /users endpoint.
+    Validate a list of all users is returned on a GET request to /users endpoint.
 
     :endpoint:  ~/v1/user
     :method:    GET
     :params:    None
     :response:  A list of user objects.
     """
-    expected = users(datetime_as_string=True)
+    expected = users(size=size, datetime_as_string=True)
 
+    client = client_factory(size=size)
     # Make request and gather response.
     res: Response = client.get("/v1/users")
 
