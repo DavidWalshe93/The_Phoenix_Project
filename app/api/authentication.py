bd2d9f910ce28c048efcaa16380df60d9302ba4a
DavidWalshe93 <david.walshe93@gmail.com>
2021-05-13 19:56:07 +0100

    Added authentication for tokens and passwords.

--- app/api/authentication.py
+++ app/api/authentication.py
@@ -17,18 +17,45 @@ logger = logging.getLogger(__name__)
 
 
 @auth.verify_password
-def verify_password(email: str, password: str) -> bool:
+def verify_password(email_or_token: str, password: str) -> bool:
     """
-    Verifies a user's email and password combination.
+    Verifies a user's email and password combination or validates a authentication token assigned by the user.
 
-    :param email: The user email to identify and check credentials for.
+    :param email_or_token: The user email to identify and check credentials for or an authentication token.
     :param password: The user password to verify.
     :return: True if credentials were correct else False.
     """
-    # Check if the user supplied a email, if anonymous user, return false.
-    if email == "":
+    # Check if the user supplied a email or a token, if anonymous user, return false.
+    if email_or_token == "":
         return False
 
+    if password == "":
+        return token_auth(token=email_or_token)
+
+    return password_auth(email=email_or_token, password=password)
+
+
+def token_auth(token: str) -> bool:
+    """
+    Setup the session with token authentication.
+
+    :param token: The token supplied by the client.
+    :return: True if the token identified user is not None, else False.
+    """
+    g.current_user = User.verify_auth_token(token)
+    g.token_used = True
+
+    return g.current_user is not None
+
+
+def password_auth(email: str, password: str) -> bool:
+    """
+    Setup the session with email/password authentication.
+
+    :param email: The email address supplied by the user.
+    :param password: The password supplied by the user.
+    :return: If the email/password combination is valid.
+    """
     # Get the user information from the DB.
     user = User.query.filter_by(email=email).first()
 
@@ -38,6 +65,7 @@ def verify_password(email: str, password: str) -> bool:
 
     # Add the user to the Flask globals for this session.
     g.current_user = user
+    g.token_used = False
 
     # Check if the users password is correct.
     return user.verify_password(password)
