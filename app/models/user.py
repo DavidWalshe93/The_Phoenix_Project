ab79b7c0008ea4933210266ed988a2527a7c9066
DavidWalshe93 <david.walshe93@gmail.com>
2021-05-13 17:03:30 +0100

    Starting on authentication.py

--- app/models/user.py
+++ app/models/user.py
@@ -5,13 +5,18 @@ Date:       11 May 2021
 
 import logging
 
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask_httpauth import HTTPBasicAuth
+from itsdangerous import Serializer
 
 from .. import db
 from .. import login_manager
 
 logger = logging.getLogger(__name__)
+
+auth = HTTPBasicAuth()
 
 
 class User(UserMixin, db.Model):
@@ -24,6 +29,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String, unique=True)
     password_hash = db.Column(db.String)
     last_login = db.Column(db.DateTime)
+
+    # ======================================================================================================================
+    # Password Handling
+    # ======================================================================================================================
 
     @property
     def password(self):
@@ -43,6 +52,29 @@ class User(UserMixin, db.Model):
         :return: True if password is correct, False otherwise.
         """
         return check_password_hash(self.password_hash, password)
+
+    # ======================================================================================================================
+    # Token Handling
+    # ======================================================================================================================
+
+    def generate_auth_token(self, expiration: int):
+        s = Serializer(current_app.config["SECRET_KEY"], expires_in=expiration)
+
+        return s.dumps({"id": self.id}).decode("uft-8")
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config["SECRET"])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+
+        return User.query.get(data['id'])
+
+    # ======================================================================================================================
+    # Helpers
+    # ======================================================================================================================
 
     def as_dict(self) -> dict:
         """
