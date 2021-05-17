dcbe43be78076aea65c54ee5d27af1adfcd29aa4
DavidWalshe93 <david.walshe93@gmail.com>
2021-05-17 01:52:06 +0100

    Addded more tests for deleted and retrieve API calls.

--- tests/functional/user/test_delete.py
+++ tests/functional/user/test_delete.py
@@ -4,21 +4,22 @@ Date:       15 May 2021
 """
 
 import json
+from copy import deepcopy
 
+import pytest
 from flask import Response
 
 from tests.functional.utils import FlaskTestRig, login, token_auth_header_token, datetime_as_string
 
 
 @FlaskTestRig.setup_app(n_users=3)
-def test_get_user_me_no_auth(client_factory, make_users, **kwargs):
+def test_delete_me_no_auth_401(client_factory, make_users, **kwargs):
     """
-    Validate an Unauthorised error is returned when attempting to get
-    a users information on a GET request to /api/v1/user/me endpoint without being
-    authenticated.
+    Validate an Unauthorised error is returned when attempting to delete
+    the current user.
 
     :endpoint:  /api/v1/users/me
-    :method:    GET
+    :method:    DELETE
     :auth:      False
     :params:    None
     :status:    401
@@ -43,15 +44,14 @@ def test_get_user_me_no_auth(client_factory, make_users, **kwargs):
 
 
 @FlaskTestRig.setup_app(n_users=3)
-def test_get_user_me_with_auth(client_factory, make_users, **kwargs):
+def test_delete_me_with_auth_user_200(client_factory, make_users, **kwargs):
     """
-    Validate the current user's information is returned on a
-    GET request to /users endpoint.
+    Validate the current user can close their account.
 
-    :endpoint:  /api/v1/user/me
-    :method:    GET
-    :auth:      True
-    :params:    Auth Token
+    :endpoint:  /api/v1/users/me
+    :method:    DELETE
+    :auth:      True (Token)
+    :params:    None
     :status:    200
     :response:  A list of user objects.
     """
@@ -71,3 +71,115 @@ def test_get_user_me_with_auth(client_factory, make_users, **kwargs):
     assert res.status_code == 200
 
     login(rig.client, user, should_fail=True)
+
+
+@FlaskTestRig.setup_app(n_users=3)
+def test_delete_users_with_auth_user_401(client_factory, make_users, **kwargs):
+    """
+    Validate a User role cannot bulk delete.
+
+    :endpoint:  /api/v1/users
+    :method:    DELETE
+    :auth:      True (Token)
+    :params:    None
+    :status:    401
+    :response:  401 error and message.
+    """
+    rig: FlaskTestRig = FlaskTestRig.extract_rig_from_kwargs(kwargs)
+
+    expected = {
+        'error': 'Unauthorised',
+        'message': 'Invalid credentials.'
+    }
+
+    # Acquire login token for first user.
+    user = rig.get_first_user(keep_password=True)
+    token = login(rig.client, user)
+
+    # Make request and gather response.
+    res: Response = rig.client.delete("/api/v1/users", headers=token_auth_header_token(token))
+
+    # Verify response matches expected.
+    assert json.loads(res.data) == expected
+    assert res.status_code == 401
+
+
+@FlaskTestRig.setup_app(n_users=10)
+def test_delete_users_with_auth_admin_200(client_factory, make_users, **kwargs):
+    """
+    Validate a Admin role can bulk delete.
+
+    :endpoint:  /api/v1/users
+    :method:    DELETE
+    :auth:      True (Token)
+    :params:    None
+    :status:    200
+    :response:  id and username of deleted users.
+    """
+    rig: FlaskTestRig = FlaskTestRig.extract_rig_from_kwargs(kwargs)
+
+    users_to_delete = {
+        "users": [
+            {"id": 0},
+            {"id": 2}
+        ]
+    }
+
+    expected = rig.get_current_users(keep_email=True, keep_password=True)
+    expected = [expected[0], expected[2]]
+    expected_full = deepcopy(expected)
+    [item.pop("last_login") for item in expected]
+    [item.pop("email") for item in expected]
+    [item.pop("password") for item in expected]
+
+    # Acquire login token for first user.
+    user = rig.get_first_user(keep_password=True, admin_only=True)
+    token = login(rig.client, user)
+
+    # Make request and gather response.
+    res: Response = rig.client.delete("/api/v1/users",
+                                      headers=token_auth_header_token(token),
+                                      data=json.dumps(users_to_delete))
+
+    # Verify response matches expected.
+    assert json.loads(res.data) == expected
+    assert res.status_code == 200
+
+    for user in expected_full:
+        login(rig.client, user, should_fail=True)
+
+
+@FlaskTestRig.setup_app(n_users=10)
+@pytest.mark.parametrize("user_id", [3, 4, 5, 6, 7])
+def test_delete_user_id_with_auth_admin_200(user_id, client_factory, make_users, **kwargs):
+    """
+    Validate a User role cannot bulk delete.
+
+    :endpoint:  /api/v1/users
+    :method:    DELETE
+    :auth:      True (Token)
+    :params:    None
+    :status:    200
+    :response:  The username and id of the deleted user.
+    """
+    rig: FlaskTestRig = FlaskTestRig.extract_rig_from_kwargs(kwargs)
+
+    expected = rig.get_current_users(keep_email=True, keep_password=True)[user_id]
+    expected_full = deepcopy(expected)
+    _ = [expected.pop(item) for item in ["email", "last_login", "password"]]
+
+    print(expected)
+
+    # Acquire login token for first user.
+    user = rig.get_first_user(keep_password=True, admin_only=True)
+    token = login(rig.client, user)
+
+    # Make request and gather response.
+    res: Response = rig.client.delete(f"/api/v1/users/{user_id}",
+                                      headers=token_auth_header_token(token))
+
+    # Verify response matches expected.
+    assert json.loads(res.data) == [expected]
+    assert res.status_code == 200
+
+    login(rig.client, expected_full, should_fail=True)
--- tests/functional/user/test_retrieve.py
+++ tests/functional/user/test_retrieve.py
@@ -80,10 +80,10 @@ def test_get_user_me_with_auth_user(client_factory, make_users, **kwargs):
 
 @FlaskTestRig.setup_app(n_users=4)
 @pytest.mark.parametrize("user_id", [1, 2, 3])
-def test_get_users_with_auth_user(user_id, client_factory, make_users, **kwargs):
+def test_get_user_with_id_with_auth_user(user_id, client_factory, make_users, **kwargs):
     """
     Validate the current user's information is returned on a
-    GET request to /users endpoint.
+    GET request to /users endpoint as a User.
 
     :endpoint:  /api/v1/user/<int:id>
     :method:    GET
@@ -97,6 +97,8 @@ def test_get_users_with_auth_user(user_id, client_factory, make_users, **kwargs)
 
     # The expected user information to be returned.
     expected = rig.get_current_users(keep_email=False)[user_id]
+
+    print(expected)
 
     # Acquire login token for first user.
     user = rig.get_first_user(keep_password=True)
@@ -114,3 +116,111 @@ def test_get_users_with_auth_user(user_id, client_factory, make_users, **kwargs)
     # Verify response matches expected.
     assert data == expected
     assert res.status_code == 200
+
+
+@FlaskTestRig.setup_app(n_users=3)
+def test_get_user_with_id_with_auth_user_404(client_factory, make_users, **kwargs):
+    """
+    Validate that a 404 error is raised when a non-existing id is queried
+    against the database as a User.
+
+    :endpoint:  /api/v1/user/<int:id>
+    :method:    GET
+    :auth:      True
+    :params:    Auth Token, A user ID to get information on.
+    :status:    404
+    :response:  404 error due to user not existing.
+    """
+    rig: FlaskTestRig = FlaskTestRig.extract_rig_from_kwargs(kwargs)
+
+    user_id = 100
+
+    expected = {'error': 'Not Found', 'message': 'User does not exist.'}
+
+    # Acquire login token for first user.
+    user = rig.get_first_user(keep_password=True)
+    token = login(rig.client, user)
+
+    # Make request and gather response.
+    res: Response = rig.client.get(f"/api/v1/users/{user_id}", headers=token_auth_header_token(token))
+
+    # Get JSON data returned.
+    data = json.loads(res.data)
+
+    # Verify response matches expected.
+    assert data == expected
+    assert res.status_code == 404
+
+
+@FlaskTestRig.setup_app(n_users=4)
+@pytest.mark.parametrize("user_id", [1, 2, 3])
+def test_get_users_with_auth_admin(user_id, client_factory, make_users, **kwargs):
+    """
+    Validate the a user keyed by their ID is returned on a
+    GET request to /users endpoint as an Admin.
+
+    :endpoint:  /api/v1/user/<int:id>
+    :method:    GET
+    :auth:      True
+    :params:    Auth Token, A user ID to get information on.
+    :status:    200
+    :response:  A object describing a user given the user's id,
+                including the id, username and last_login time.
+    """
+    rig: FlaskTestRig = FlaskTestRig.extract_rig_from_kwargs(kwargs)
+
+    # The expected user information to be returned.
+    expected = rig.get_current_users(keep_email=True, keep_role_name=user_id)[user_id]
+
+    print(expected)
+
+    # Acquire login token for first user.
+    user = rig.get_first_user(keep_email=True, admin_only=True)
+    token = login(rig.client, user)
+
+    # Make request and gather response.
+    res: Response = rig.client.get(f"/api/v1/users/{user_id}", headers=token_auth_header_token(token))
+
+    # Get JSON data returned.
+    data = json.loads(res.data)
+
+    # Add the last_login field to the expected data.
+    expected = [{**user, "last_login": datetime_as_string(user["last_login"])} for user in [expected]][0]
+
+    # Verify response matches expected.
+    assert data == expected
+    assert res.status_code == 200
+
+
+@FlaskTestRig.setup_app(n_users=3)
+def test_get_user_with_id_with_auth_admin_404(client_factory, make_users, **kwargs):
+    """
+    Validate that a 404 error is raised when a non-existing id is queried
+    against the database as a Admin.
+
+    :endpoint:  /api/v1/user/<int:id>
+    :method:    GET
+    :auth:      True
+    :params:    Auth Token, A user ID to get information on.
+    :status:    404
+    :response:  404 error due to user not existing.
+    """
+    rig: FlaskTestRig = FlaskTestRig.extract_rig_from_kwargs(kwargs)
+
+    user_id = 100
+
+    expected = {'error': 'Not Found', 'message': 'User does not exist.'}
+
+    # Acquire login token for first user.
+    user = rig.get_first_user(keep_password=True, admin_only=True)
+    token = login(rig.client, user)
+
+    # Make request and gather response.
+    res: Response = rig.client.get(f"/api/v1/users/{user_id}", headers=token_auth_header_token(token))
+
+    # Get JSON data returned.
+    data = json.loads(res.data)
+
+    # Verify response matches expected.
+    assert data == expected
+    assert res.status_code == 404
