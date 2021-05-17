bdf303c02051f627984631b7ef56206279dc310d
DavidWalshe93 <david.walshe93@gmail.com>
2021-05-17 03:50:30 +0100

    Added Codacy fixes.

--- app/api/v1/handlers/base.py
+++ app/api/v1/handlers/base.py
@@ -7,7 +7,6 @@ import logging
 from abc import abstractmethod
 
 from app.models.user import User
-from app.api.authentication import auth, Access
 from app.api.errors import bad_request, not_found, unauthorized
 from app.api.v1.schema import UserSchema, ValidationError
 
